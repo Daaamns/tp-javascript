105734ab2ad0b95e389793556497c3ea18fa0e83
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-02-26 11:57:24 +0100

    first commit

---
+++ TP--JS/tp02.js
@@ -0,0 +1,27 @@
+let num1 = 2;
+let num2 = 4;
+
+console.log("test opération");
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+console.log("test comparaison");
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 == num2);
+console.log(num1 != num2);
+
+let b1 = true;
+let b2 = false;
+
+console.log("test true - false");
+
+console.log(b1 && b2);
+console.log(b1 || b2);
+console.log(!b1);
+console.log(b2);
